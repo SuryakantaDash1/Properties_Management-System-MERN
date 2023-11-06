6b0fee9e8ef30004b30e5207895a099bad572e1d
Suryakanta Dash <sdash8273@gmail.com>
2023-11-06 12:15:07 +0530

    create search api route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -63,6 +63,8 @@ export const getListing = async (req, res, next) => {
   }
 };
 
+
+
 export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
