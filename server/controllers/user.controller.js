1b984496c64e411bc1f65165c22bcf8790fd48d5
Suryakanta Dash <sdash8273@gmail.com>
2023-11-06 11:36:15 +0530

    Add contact landlord functionality

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -65,17 +65,18 @@ export const updateUser = async (req, res, next) => {
     }
   };
   
-//   export const getUser = async (req, res, next) => {
-//     try {
+  
+  export const getUser = async (req, res, next) => {
+    try {
       
-//       const user = await User.findById(req.params.id);
+      const user = await User.findById(req.params.id);
     
-//       if (!user) return next(errorHandler(404, 'User not found!'));
+      if (!user) return next(errorHandler(404, 'User not found!'));
     
-//       const { password: pass, ...rest } = user._doc;
+      const { password: pass, ...rest } = user._doc;
     
-//       res.status(200).json(rest);
-//     } catch (error) {
-//       next(error);
-//     }
-//   };
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUser, getUserListings, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, getUserListings, test, updateUser, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -9,6 +9,7 @@ router.get('/test', test);
 router.post('/update/:id', verifyToken,  updateUser);
 router.delete('/delete/:id', verifyToken,  deleteUser);
 router.get('/listings/:id', verifyToken, getUserListings);
+router.get('/:id', verifyToken, getUser);
 
 
 export default router;
